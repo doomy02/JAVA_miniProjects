2b3f07cc17bd20a60ea6147acb464090009e90ad
Sebastian Stăvar <60173965+doomy02@users.noreply.github.com>
2022-10-02 11:58:29 +0300

    Add files via upload

---
+++ NumberGuesser.java
@@ -0,0 +1,39 @@
+package ghwgweg;
+
+import java.util.Scanner;
+
+public class NumberGuesser {
+
+	public static int getRandomNumber(int min, int max) {
+		return (int) ((Math.random() * (max - min)) + min);
+	}
+
+	public static void main(String[] args) {
+		int no = getRandomNumber(1, 100);
+		int tries = 0;
+		boolean found = false;
+
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Guess the number!");
+
+		while (found == false) {
+			System.out.print("Your guess: ");
+			int number = scan.nextInt();
+			tries++;
+
+			if (number == no) {
+				System.out.println();
+				System.out.println("GOOD JOB, you found the number!");
+				found = true;
+			} else if (number < no) {
+				System.out.println("The number is higher!");
+			} else if (number > no) {
+				System.out.println("The number is lower!");
+			}
+
+		}
+		System.out.println("Tries: " + tries);
+
+	}
+
+}
---
+++ RockPaperScissors.java
@@ -0,0 +1,97 @@
+package ghwgweg;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class RockPaperScissors {
+
+	final static String[] str = { "Rock", "Paper", "Scissors" };
+
+	public static String getRandom(String[] str) {
+		Random random = new Random();
+		return str[random.nextInt(str.length)];
+	}
+
+	public static void main(String[] args) {
+		boolean playing = true;
+		int scorePC = 0;
+		int scorePlayer = 0;
+		int scoreDraws = 0;
+
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Let's play Rock/Paper/Scissors!");
+		System.out.println("Press 'Q' if you want to quit!");
+		System.out.println();
+
+		while (playing == true) {
+			System.out.print("Your choise: ");
+			String choise = scan.nextLine();
+			String PC = getRandom(str);
+
+			if (choise.equals("Q")) {
+				playing = false;
+			} else if (choise.equals("Rock") && PC.equals("Scissors")) {
+				System.out.println("Player: " + choise + ", PC: " + PC);
+				System.out.println("Player Won!");
+				System.out.println();
+
+				scorePlayer++;
+			} else if (choise.equals("Rock") && PC.equals("Paper")) {
+				System.out.println("Player: " + choise + ", PC: " + PC);
+				System.out.println("PC Won!");
+				System.out.println();
+
+				scorePC++;
+			} else if (choise.equals("Rock") && PC.equals("Rock")) {
+				System.out.println("Player: " + choise + ", PC: " + PC);
+				System.out.println("Draw!");
+				System.out.println();
+
+				scoreDraws++;
+			} else if (choise.equals("Paper") && PC.equals("Rock")) {
+				System.out.println("Player: " + choise + ", PC: " + PC);
+				System.out.println("Player Won!");
+				System.out.println();
+
+				scorePlayer++;
+			} else if (choise.equals("Paper") && PC.equals("Scissors")) {
+				System.out.println("Player: " + choise + ", PC: " + PC);
+				System.out.println("PC Won!");
+				System.out.println();
+
+				scorePC++;
+			} else if (choise.equals("Paper") && PC.equals("Paper")) {
+				System.out.println("Player: " + choise + ", PC: " + PC);
+				System.out.println("Draw!");
+				System.out.println();
+
+				scoreDraws++;
+			} else if (choise.equals("Scissors") && PC.equals("Paper")) {
+				System.out.println("Player: " + choise + ", PC: " + PC);
+				System.out.println("Player Won!");
+				System.out.println();
+
+				scorePlayer++;
+			} else if (choise.equals("Scissors") && PC.equals("Rock")) {
+				System.out.println("Player: " + choise + ", PC: " + PC);
+				System.out.println("PC Won!");
+				System.out.println();
+
+				scorePC++;
+			} else if (choise.equals("Scissors") && PC.equals("Scissors")) {
+				System.out.println("Player: " + choise + ", PC: " + PC);
+				System.out.println("Draw!");
+				System.out.println();
+
+				scoreDraws++;
+			} else {
+				System.out.println("Wrong input! Try again!");
+			}
+
+		}
+		System.out.println();
+		System.out.println("Thanks for playing!");
+		System.out.println("Player's score: " + scorePlayer + ", PC's score: " + scorePC + ", Draws: " + scoreDraws);
+	}
+
+}
